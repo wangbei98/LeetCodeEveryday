e4523cf102e72a90ba983bf93d828414289dc2be
w865629524 <424113477@qq.com>
2020-10-18 00:09:04 +0800

    19

---
+++ wanglq/每日打卡/2020.10.18-19.删除链表的倒数第n个节点.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=cpp
+ *
+ * [19] 删除链表的倒数第N个节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* p1 = head;
+        ListNode* p2 = head;
+        while (n--) p1 = p1->next;
+        if (!p1) return p2->next;
+        while (p1->next) p1 = p1->next, p2 = p2->next;
+        p2->next = p2->next->next;
+        return head;
+    }
+};
+// @lc code=end
+
