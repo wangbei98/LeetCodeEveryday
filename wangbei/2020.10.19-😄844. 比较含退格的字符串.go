5bca57a5f64ede2e1b60003bb5571e53e7ef8540
wangbei98 <beiwang121@163.com>
2020-10-19 17:52:39 +0800

    10.19

---
+++ wangbei/2020.10.19-😄844. 比较含退格的字符串.go
@@ -0,0 +1,15 @@
+func build(str string) string {
+	s := []byte{}
+	for i := range str {
+		if str[i] != '#' {
+			s = append(s, str[i])
+		} else if len(s) > 0 {
+			s = s[:len(s)-1]
+		}
+	}
+	return string(s)
+}
+
+func backspaceCompare(s, t string) bool {
+	return build(s) == build(t)
+}
