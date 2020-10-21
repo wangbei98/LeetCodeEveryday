1b17d67b1cefdaeb87f103b88c2c5a51d4421bef
VinceWangw <37650088+VinceWangw@users.noreply.github.com>
2020-10-21 12:04:44 +0800

    Update 20201021-925. 长按键入.java

--- wangfeng/20201021-925. 长按键入.java
+++ wangfeng/20201021-925. 长按键入.java
@@ -3,24 +3,18 @@ class Solution {
 
         int i = 0;
         int j = 0;
-        if(name.charAt(0) != typed.charAt(0)) return false;
-        while(i < name.length() && j < typed.length()){
-            if(name.charAt(i) == typed.charAt(j)){
+        
+        while(j < typed.length()){
+            if(i < name.length() && name.charAt(i) == typed.charAt(j)){
                 i ++;
                 j ++;
-            }else if(typed.charAt(j) == typed.charAt(j - 1)){
+            }else if(j > 0 && typed.charAt(j) == typed.charAt(j - 1)){
                 j ++;
             }else{
                 return false;  
             }   
         }
-
-        while(j < typed.length()){
-            char c = typed.charAt(j - 1);
-            if(typed.charAt(j ++) != c){
-                return false;
-            }
-        }
+        
         return i == name.length() ;
     }
 }
